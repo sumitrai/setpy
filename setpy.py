8a35e7468b03ed70990cb62b312e6d8f02f60aba
Sumit Rai <sumitrai96@gmail.com>
2013-08-03 21:48:28 +0530

    Fixed few bugs in function setpy.isReflexive. 1. It now check if relation R is indeed a valid realation on set A before making check for reflexive relation. 2. Identity realtion check shoud be on R not A.

--- setpy.py
+++ setpy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python -tt
 # Description: Python Module to implement many functions related to set and relations.
 
-# Checks is R is a relation on set A and B
+# Checks is set R is a valid relation on set A and B
 # return True if R is a valid relation on A and B
 # else return False
 def isRelation(R, A, B):
@@ -22,11 +22,16 @@ def identity(A):
 	return I
 	
 # Checks if a given set is reflexive
+# return True is set R is reflexive relation on set A
 def isReflexive(R, A):
-	# For R to be a reflexive relation on A,
-	# Identity Relation on A must be a subset of R
+	# if R is not a valid relation on A
+	# return False
+	if not isRelation(R, A, A):
+		return False	
+	# Now, For R to be a reflexive relation on set A,
+	# Identity Relation on set A must be a subset of R
 	I = identity(A)
-	if I.issubset(A):
+	if I.issubset(R):
 		return True
 	else:
 		return False
