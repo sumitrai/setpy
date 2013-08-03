09749e6a7d9f709acb791a2444927eb852e65750
Sumit Rai <sumitrai96@gmail.com>
2013-08-03 20:43:08 +0530

    second commit

---
+++ set.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python -tt
+# Description: Module to implement many functions related to set.
+
+# Checks is R is a relation on set A and B
+# return True if R is a valid relation on A and B
+# else return False
+def isRelation(R, A, B) {
+	import itertools as i
+	# For R to be a realtion on A and B
+	# R must ge a subset of A * B
+	AxB = set(i.product(A,B))
+	if R.issubset(AxB):
+		return True
+	else:
+		return False
+}
+
+# Return Identity Relation on a given set A
+def identity(A) {
+	I = set();
+	for element in A:
+		I.add((element,element))
+	return I
+}
+	
+# Checks if a given set is reflexive
+def isReflexive(R, A) {
+	# For R to be a reflexive relation on A,
+	# Identity Relation on A must be a subset of R
+	I = identity(A)
+	if I.issubset(A):
+		return True
+	else
+		return False
+}	
