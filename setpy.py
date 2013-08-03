d860c6e6bec7106b38d2becafc33db37c3004081
Sumit Rai <sumitrai96@gmail.com>
2013-08-03 21:19:13 +0530

    Renamed set.py to setpy.py to avoid confusion with python inbuilt type set. Fixed some syntax errors.

--- setpy.py
+++ setpy.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python -tt
-# Description: Module to implement many functions related to set.
+# Description: Python Module to implement many functions related to set and relations.
 
 # Checks is R is a relation on set A and B
 # return True if R is a valid relation on A and B
 # else return False
-def isRelation(R, A, B) {
+def isRelation(R, A, B):
 	import itertools as i
 	# For R to be a realtion on A and B
 	# R must ge a subset of A * B
@@ -13,23 +13,20 @@ def isRelation(R, A, B) {
 		return True
 	else:
 		return False
-}
 
 # Return Identity Relation on a given set A
-def identity(A) {
+def identity(A):
 	I = set();
 	for element in A:
 		I.add((element,element))
 	return I
-}
 	
 # Checks if a given set is reflexive
-def isReflexive(R, A) {
+def isReflexive(R, A):
 	# For R to be a reflexive relation on A,
 	# Identity Relation on A must be a subset of R
 	I = identity(A)
 	if I.issubset(A):
 		return True
-	else
+	else:
 		return False
-}	
